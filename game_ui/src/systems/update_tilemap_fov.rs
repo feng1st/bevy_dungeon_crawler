e299ea1d161cd2708258c3e30ffc17b87dcac0df
feng1st <nf_xp+github@hotmail.com>
2023-12-13 11:06:51 +0800

    Add spatial memory

--- game_ui/src/systems/update_tilemap_fov.rs
+++ game_ui/src/systems/update_tilemap_fov.rs
@@ -9,7 +9,7 @@ pub fn update_tilemap_fov(
         (With<Player>, Changed<FieldOfView>),
     >,
     mut tilemap_query: Query<&mut TileStorage, Without<Delete>>,
-    mut tile_query: Query<&mut TileVisible>,
+    mut tile_query: Query<(&mut TileVisible, &mut TileColor)>,
 ) {
     let Ok(tile_storage) = tilemap_query.get_single_mut() else {
         return;
@@ -29,16 +29,17 @@ pub fn update_tilemap_fov(
     for pos in old_set {
         let tile_pos = TilePos::new(pos.x as u32, pos.y as u32);
         if let Some(tile_entity) = tile_storage.get(&tile_pos) {
-            let mut visibility = tile_query.get_mut(tile_entity).unwrap();
-            visibility.0 = false;
+            let (_, mut tile_color) = tile_query.get_mut(tile_entity).unwrap();
+            *tile_color = Color::GRAY.into();
         }
     }
 
     for pos in new_set {
         let tile_pos = TilePos::new(pos.x as u32, pos.y as u32);
         if let Some(tile_entity) = tile_storage.get(&tile_pos) {
-            let mut visibility = tile_query.get_mut(tile_entity).unwrap();
-            visibility.0 = true;
+            let (mut tile_visibility, mut tile_color) = tile_query.get_mut(tile_entity).unwrap();
+            tile_visibility.0 = true;
+            *tile_color = Color::WHITE.into();
         }
     }
 
