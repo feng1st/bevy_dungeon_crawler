9ce6ee97d0222fb4483aae3936ac3586d7d6a38d
feng1st <nf_xp+github@hotmail.com>
2023-12-16 16:28:48 +0800

    Update tooltip

--- game_core/src/systems/spawn_amulet_of_yala.rs
+++ game_core/src/systems/spawn_amulet_of_yala.rs
@@ -6,5 +6,10 @@ pub fn spawn_amulet_of_yala(mut commands: Commands, query: Query<&AmuletStart>) 
     let Ok(amulet_start) = query.get_single() else {
         return;
     };
-    commands.spawn((Item, AmuletOfYala, MapPos(amulet_start.0)));
+    commands.spawn((
+        Item,
+        AmuletOfYala,
+        Name("Amulet of Yala".to_string()),
+        MapPos(amulet_start.0),
+    ));
 }
