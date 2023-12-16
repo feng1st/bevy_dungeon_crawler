8b69b406a4da4f2f8c624dc451f276e12c5795f4
feng1st <nf_xp+github@hotmail.com>
2023-12-16 23:32:18 +0800

    Disable resting

--- game_core/src/systems/take_rest.rs
+++ game_core/src/systems/take_rest.rs
@@ -8,7 +8,7 @@ pub fn take_rest(
 ) {
     for action in take_rest_event_reader.read() {
         if let Ok((mut rest, mut health)) = actor_query.get_mut(action.actor) {
-            rest.current += 1;
+            // rest.current += 1;
             if rest.current >= rest.max {
                 rest.current = 0;
                 health.current = i32::min(health.current + 1, health.max);
